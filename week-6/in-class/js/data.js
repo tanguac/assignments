1802d8bd162faf55dbb3908735f27e9889ccabb1
Benjamin Florin <benjamin.florin@gmail.com>
2018-03-20 15:17:41 -0400

    Added week-6 assignments

---
+++ week-6/in-class/js/data.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let boston_library = "Boston Public Library";
+let concord_library = "Concord Free Public Library";
+let hanover_library = "John Curtis Free Library";
+let simmons_library = "Beatley Library";
+
+let call_numbers = [
+    "PQ2246.M2 E5",
+    "PR2759 .P3",
+    "PR6056.A75 T76x",
+    "PS1744.G57 Y4534",
+    "PR2810.A2 M38",
+    "PS1284 .K53",
+    "PQ2662 .A6523 E44",
+    "PR5815 .I47 K5",
+    "PL818.I7 K513",
+    "PR2870.A2 P6"
+];
+
+let hours = [
+    "Monday: 9a - 9p",
+    "Tuesday: 9a - 9p",
+    "Wednesday: 9a - 9p",
+    "Thursday: 9a - 9p",
+    "Friday: 9a - 5p",
+    "Saturday: 9a - 5p",
+    "Sunday: closed"
+];
