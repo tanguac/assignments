d52bbd65dfdcd49d7ce033bcf6a37253512970c4
ctanguay <ctanguay@mit.edu>
2018-03-20 20:52:46 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,51 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let my_name = "Chris";
+let your_name = "Rich";
+
+if (my_name === "Chris") {
+  renderOutput("That's me!");
+} else if (my_name === "Rich") {
+  renderOutput("That's Rich!");
+} else {
+  renderOutput("I must be someone else");
+}
+
+//for (var index=0; index<10; index++) {renderOutput(index); }
+
+function helloWorld() {
+  renderOutput("Hello World");
+}
+
+helloWorld();
+helloWorld();
+helloWorld();
+
+function helloName(name) {
+  renderOutput("Hello "+name);
+}
+
+helloName("Ben");
+helloName("George");
+helloName("Martha");
+
+function todaysHours(days_hours){
+  renderOutput(days_hours);
+}
+
+//todaysHours("Tuesday: 9a - 5p");
+hours.forEach(todaysHours);
+
+//function classifyCallNumbers(callNo){
+  //if callNo.startsWith("PQ"){
+ //   let lit_type="French";
+//  } else if callNo.startsWith("PR") {
+ //   let lit_type="English";
+//  } else {
+//    let lit_type="Other";
+//  }
+//  renderOutput(callNo+" - "+lit_type+" literature");
+  
+//}
+//all_numbers.forEach(classifyCallNumbers);
