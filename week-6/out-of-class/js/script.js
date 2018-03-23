43febbf24c9577a303f55a5bc7f1d584d73ff33e
ctanguay <ctanguay@mit.edu>
2018-03-22 20:39:59 -0400

    dailyHours() assignment

--- week-6/out-of-class/js/script.js
+++ week-6/out-of-class/js/script.js
@@ -4,3 +4,26 @@ function renderOutput(output) {
     let output_element = document.getElementById("output");
     output_element.innerHTML += "<div>" + output + "</div>"
 }
+
+let hours = [
+    "Monday: 9a - 9p",
+    "Tuesday: 9a - 9p",
+    "Wednesday: 9a - 9p",
+    "Thursday: 9a - 9p",
+    "Friday: 9a - 5p",
+    "Saturday: 9a - 5p",
+    "Sunday: closed"
+];
+var i;
+for (i=0; i<hours.length; i++) {
+    dailyHours(hours[i]);
+}
+
+function dailyHours(hours) {
+  let parts=hours.split(": ");
+  if (parts[1]==="closed") {
+    renderOutput("On "+parts[0]+" the library is closed");
+  } else {
+    renderOutput("On "+parts[0]+" the hours are "+parts[1]);
+  }
+}
